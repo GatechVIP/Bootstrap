295b0a1d149d45c0710f71138bd5974c6108b140
eeason3 <e.eason@gatech.edu>
2015-11-06 12:21:50 -0500

    index.html uses local bootstrap file

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,13 +7,6 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('./css/'));
 });
 
-gulp.task('bootstrap_styles', function() {
-	gulp.src('bower_components/bootstrap-sass/assets/stylesheets/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('stylesheets/'));
-});
-
-
 gulp.task('default',function() {
     gulp.watch('sass/**/*.scss',['styles']);
 });
